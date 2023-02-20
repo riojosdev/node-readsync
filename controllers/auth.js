33aa50233f5d54cdf5ca61f0221b3a4b7410d8ce
riojos <riojosrj@gmail.com>
2023-02-20 11:17:51 +0530

    initial integration to google calendar api

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const { google } = require('googleapis')
+const { scopes, oauth2Client } = require('../config/google')
+
 const { Op } = require('sequelize')
 // const { v4: uuid } = require('uuid')
 const creatError = require('http-errors')
@@ -29,9 +32,9 @@ exports.register = async (req, res, next) => {
 		// insert data to `users` table
 		// const query = `// INSERT INTO users (fname, lname, mobile, email, publicID, privateID, dob) // VALUES ('${data.fname}', '${data.lname}', '${data.mobile}', '${data.email}', '${vapidKeys.publicKey}', '${vapidKeys.privateKey}', '${data.dob}')`
 
-		
-		const user = await db.User.create({ 
-			fname: data.fname, 
+
+		const user = await db.User.create({
+			fname: data.fname,
 			lname: data.lname,
 			mobile: data.mobile,
 			email: data.email,
@@ -91,7 +94,7 @@ exports.login = async (req, res) => {
 			token = await tokenHandler({ id, lname, email, publicID })
 
 			const object = { fname, lname, email, dob, privateID, publicID, mobile, id, token }
-			console.log({object})
+			console.log({ object })
 			output = { user: object, token, email }
 
 			return output
@@ -136,4 +139,80 @@ exports.login = async (req, res) => {
 // exports.entry = async (req, res, next) => {
 exports.entry = async (req, res) => {
 	res.render('index')
+}
+
+
+exports.googleOAuth = async (req, res) => {
+	const url = oauth2Client.generateAuthUrl({
+		// 'online' (default) or 'offline' (gets refresh_token)
+		access_type: 'offline',
+
+		// If you only need one scope you can pass it as a string
+		scope: scopes
+	})
+	res.redirect(url)
+}
+
+exports.googleSync = async (req, res) => {
+	// console.log({a: req.params, b: req.query});
+	const code = req.query.code
+	const { tokens } = await oauth2Client.getToken(code)
+	console.log({ tokens })
+	oauth2Client.setCredentials(tokens)
+	const calendar = google.calendar({
+		version: 'v3',
+		auth: oauth2Client
+	})
+
+	let eventStartTime = new Date()
+	console.log({ a: eventStartTime.getDay() })
+
+	eventStartTime.setDate(eventStartTime.getDay() + 5)
+
+	let eventEndTime = new Date()
+	eventEndTime.setDate(eventEndTime.getDay() + 5)
+	eventEndTime.setMinutes(eventEndTime.getMinutes() + 45)
+
+	const event = {
+		summary: 'Movie Night!!!',
+		location: 'Thiruvananthapuram, Kerala',
+		description: 'Grab a Popcorn, it\'s Movie Night!',
+		start: {
+			dateTime: eventStartTime,
+			timeZone: 'Asia/Kolkata',
+		},
+		end: {
+			dateTime: eventEndTime,
+			timeZone: 'Asia/Kolkata'
+		},
+		colorId: 1,
+	}
+
+	console.log({ event })
+
+
+	calendar.freebusy.query({
+		resource: {
+			timeMin: eventStartTime,
+			timeMax: eventEndTime,
+			timeZone: 'Asia/Kolkata',
+			items: [{ id: 'primary' }],
+		},
+	}, (err, res) => {
+		if (err) return console.error('Free Busy Query Error: ', err)
+
+		const eventsArr = res.data.calendars.primary.busy
+
+		if (eventsArr.length === 0) return calendar.events.insert({
+			calendarId: 'primary',
+			resource: event
+		}, err => {
+			if (err) return console.error('Calendar Event Creation Error: ', err)
+
+			return console.log('Calendar Event Created.')
+		})
+		return console.log('Sorry I\'m Busy')
+	})
+	res.send('WELCOME TO GET INBOX')
+	res.render('page')
 }
